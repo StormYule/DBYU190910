99c15e5af701528f1ad7604c68f8fdad81d0e236
Yu <yuha@umh.edu>
2019-09-11 10:23:45 -0500

    Add project files.

---
+++ Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee11]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(50) NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL
+)
---
+++ View1.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[View1]
+	AS SELECT Email, FirstName, LastName FROM [Employee11]
